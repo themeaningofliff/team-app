33183757823f4f1eb4f4484ae7df29b2e0f56fc5
Jason Michael <jason@euremir.com>
2018-02-05 22:26:19 -0500

    init

---
+++ team-appPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace teamapp
+{
+    public partial class team_appPage : ContentPage
+    {
+        public team_appPage()
+        {
+            InitializeComponent();
+            // Handle padding on iOS to fix date overlap
+            if (Device.RuntimePlatform == Device.iOS)
+                Padding = new Thickness(0, 20, 0, 0);
+        }
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if(epScore.Text != null)
+                DisplayAlert("Event created for "+eDate.Date.ToString(),
+                             "You scored "+epScore.Text.ToString()+". \n"+eoEmail.Text.ToString()+" scored "+eoScore.Text.ToString(),
+                             "OK");
+        }
+    }
+}
---
+++ team_view.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace teamapp
+{
+    public class team_view : ContentView
+    {
+        public team_view()
+        {
+            Content = new Label { Text = "Hello ContentView" };
+            NavigationPage n = new NavigationPage();
+            //n.RootPage = team_appPage;
+        }
+    }
+}
+
